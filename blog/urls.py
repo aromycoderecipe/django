bb67d573fe2f5fd8553bb1bed8a93c6a07d35bdb
aromycoderecipe <coderecipe@naver.com>
2018-05-08 17:50:25 +0900

    django app css 스타일 추가. 상세페이지 추가

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+# from blog import views
+from . import views
 
-from blog import views
 
 urlpatterns = [
     url(r'^$', views.post_list,name='post_list'),
     # localhost:
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
